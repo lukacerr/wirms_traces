68cb23bd9baee06c6a5ff8ec2c57385121f878f7
Luka Cerrutti <lukacerrutti2002@gmail.com>
2022-10-24 12:19:55 -0300

    init

--- Service/src/interfaces/IMethod.ts
+++ Service/src/interfaces/IMethod.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from 'express';
 import IResponse from 'interfaces/IResponse';
-import { isAsyncFunction } from 'util/types';
 
 export interface IMethodSettings { path?: string; aliases?: string[] }
 
@@ -8,7 +7,7 @@ export type IMethodAsync = (req: Request, res: Response) => Promise<IResponse | 
 
 export type IMethodSync = (req: Request, res: Response) => IResponse | unknown;
 
-export function methodIsAsync(m: IMethod): m is IMethodAsync { return isAsyncFunction(m); }
+export function methodIsAsync(m: IMethod): m is IMethodAsync { return 'then' in m; }
 
 type IMethod = IMethodAsync | IMethodSync;
 export default IMethod;
